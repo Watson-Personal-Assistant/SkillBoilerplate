86d3489b61a25703cbcc1eabb6ef5431ef068c1d
Offer Akrabi <offer.akrabi@il.ibm.com>
2018-06-13 10:23:46 +0300

    fixed manifest bug

--- index.js
+++ index.js
@@ -18,14 +18,15 @@ require('dotenv').config();
 if(manifest.nlu.indexOf('wcs') > -1) {
     handler.initialize();
 }
-let index = manifest.nlu.indexOf('skill');
+let newManifest = JSON.parse(JSON.stringify(manifest));
+let index = newManifest.nlu.indexOf('skill');
 if(index !== -1) {
-    manifest.nlu.splice(index, 1);
+    newManifest.nlu.splice(index, 1);
 }
-if (manifest.nlu.length < 1) {
+if (newManifest.nlu.length < 1) {
     console.error('No Nlu engines selected, you need to add the nlu engines you want to use to manifest.json nlu field')
 } else {
-    factory.getNLUs(manifest).then(updatedManifest => {
+    factory.getNLUs(newManifest).then(updatedManifest => {
         if (updatedManifest.nlu.regexp) {
             updatedManifest.intents = require('./res/nlu/intents');
         }
