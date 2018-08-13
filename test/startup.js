741a148ca48ed601b5349facb97f518ec3edb23a
Offer Akrabi <offer.akrabi@il.ibm.com>
2018-08-13 11:32:21 +0300

    added unit tests for nlu creation

---
+++ test/startup.js
@@ -0,0 +1,35 @@
+const should = require('should');
+const {server} = require('skill-sdk-nodejs');
+const skillManifest = require(process.env.skillSDKResDir + '/assets/manifest');
+const capcon = require('capture-console');
+let output = '';
+capcon.startCapture(process.stdout, function(stdout) {
+    output += stdout;
+});
+
+
+describe('Index', function() {
+    this.timeout(10000);
+
+    after(function(done) {
+        capcon.stopCapture(process.stdout);
+        done();
+    });
+
+    describe('Skill startup', function() {
+        let nlus;
+        if (skillManifest && skillManifest.nlu) {
+            nlus = skillManifest.nlu;
+        }
+        for(let engine of nlus) {
+            if(engine !== 'skill') {
+                it('Successfully creates nlu engine ' + engine, function (done) {
+                    let success = output.includes(`Nlu engine ${engine} creation has succeeded`);
+                    should.equal(success, true);
+                    done();
+                })
+            }
+        }
+
+    });
+});
